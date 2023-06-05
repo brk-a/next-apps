fadf806717b7a2fd92c4f34e99ef21ca9ba65676
brk-a <fnjakai@outlook.com>
2023-06-05 12:09:38 +0000

    hello world: returns json at `api/hello` endpoint

---
+++ nextjs-blog/pages/index.js
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>This is a sample website using Next.js</p>
+      </section>
+    </Layout>
+  )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
